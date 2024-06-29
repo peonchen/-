8ba33e5cbdb8009f8c6e7e102dc0dce9c1ca7477
peonchen <2630302161@qq.com>
2024-06-29 10:36:07 +0800

    课设-登录验证

--- czxt.py
+++ czxt.py
@@ -1,6 +1,7 @@
 import os
 import json
-
+import mysql.connector
+import hashlib
 class SimpleFileSystem:
     def __init__(self, filename):
         # 文件名
@@ -94,42 +95,77 @@ class SimpleFileSystem:
     # 列目录
     def list_directory(self):
         return list(self.current_directory.keys())
-if __name__ == "__main__":
-    fs = SimpleFileSystem("root.txt")
-    while True:
-        command = input("$ ")
-        argnum = len(command.split())
-        if command == "exit" and argnum == 1:
-            break
-        elif command.startswith("cd") and argnum == 2:
-            directory_name = command.split(" ")[1]
-            fs.change_directory(directory_name)
-        elif command.startswith("mkdir") and argnum == 2:
-            directory_name = command.split(" ")[1]
-            fs.create_directory(directory_name)
-        elif command.startswith("touch") and argnum == 3:
-            filename = command.split(" ")[1]
-            content = " ".join(command.split(" ")[2:])
-            fs.create_file(filename, content)
-        elif command.startswith("vim") and argnum == 3:
-            filename = command.split(" ")[1]
-            content = " ".join(command.split(" ")[2:])
-            fs.write_file(filename, content)        
-        elif command.startswith("ls") and argnum == 1:
-            print(fs.list_directory())
-        elif command.startswith("cat") and argnum == 2:
-            filename = command.split(" ")[1]
-            fs.print_file(filename)
-        elif command.startswith("rm") and argnum == 3:
-            if command.split(" ")[1] == "-d":
-                directory_name = command.split(" ")[2]
-                fs.delete_directory(directory_name)
-            elif command.split(" ")[1] == "-f":
-                filename = command.split(" ")[2]
-                fs.delete_file(filename)
-            else:
-                print("Invalid command.")
-        elif command.startswith("pwd") and argnum == 1:
-            print(fs.showpath())
+def md5_hash(password):
+    # 使用 MD5 加密密码
+    return hashlib.md5(password.encode()).hexdigest()
+def login():
+    username = input("请输入用户名：")
+    password = input("请输入密码：")
+    password=md5_hash(password)
+    print(password)
+    try:
+        db = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="chen2630",
+            database="czxt"
+        )
+        # 连接成功后可以执行数据库操作
+        cursor = db.cursor()
+        query = "SELECT * FROM user WHERE username = %s AND password = %s"
+        cursor.execute(query, (username, password))
+        result = cursor.fetchone() 
+        if result:
+            print(f"登录成功：欢迎 {username}!")
         else:
-            print("Invalid command.")
+            print("登录失败：账户名或密码不正确。")
+        
+        # 处理查询结果等操作
+        
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+    finally:
+        # 关闭数据库连接
+        if 'db' in locals() or 'db' in globals():
+            db.close()
+if __name__ == "__main__":          
+    login()
+# if __name__ == "__main__":
+    # fs = SimpleFileSystem("root.txt")
+    # while True:
+    #     command = input("$ ")
+    #     argnum = len(command.split())
+    #     if command == "exit" and argnum == 1:
+    #         break
+    #     elif command.startswith("cd") and argnum == 2:
+    #         directory_name = command.split(" ")[1]
+    #         fs.change_directory(directory_name)
+    #     elif command.startswith("mkdir") and argnum == 2:
+    #         directory_name = command.split(" ")[1]
+    #         fs.create_directory(directory_name)
+    #     elif command.startswith("touch") and argnum == 3:
+    #         filename = command.split(" ")[1]
+    #         content = " ".join(command.split(" ")[2:])
+    #         fs.create_file(filename, content)
+    #     elif command.startswith("vim") and argnum == 3:
+    #         filename = command.split(" ")[1]
+    #         content = " ".join(command.split(" ")[2:])
+    #         fs.write_file(filename, content)        
+    #     elif command.startswith("ls") and argnum == 1:
+    #         print(fs.list_directory())
+    #     elif command.startswith("cat") and argnum == 2:
+    #         filename = command.split(" ")[1]
+    #         fs.print_file(filename)
+    #     elif command.startswith("rm") and argnum == 3:
+    #         if command.split(" ")[1] == "-d":
+    #             directory_name = command.split(" ")[2]
+    #             fs.delete_directory(directory_name)
+    #         elif command.split(" ")[1] == "-f":
+    #             filename = command.split(" ")[2]
+    #             fs.delete_file(filename)
+    #         else:
+    #             print("Invalid command.")
+    #     elif command.startswith("pwd") and argnum == 1:
+    #         print(fs.showpath())
+    #     else:
+    #         print("Invalid command.")
